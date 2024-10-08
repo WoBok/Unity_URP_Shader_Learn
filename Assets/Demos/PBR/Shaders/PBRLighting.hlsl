0ede515ef424b222c32f5d77f923eaa5c62be3f9
WoBok <darwin.vinci.wkc@gmail.com>
2024-10-08 17:59:56 +0800

    PBRLighting.hlsl

--- Assets/Demos/PBR/Shaders/PBRLighting.hlsl
+++ Assets/Demos/PBR/Shaders/PBRLighting.hlsl
@@ -1,15 +1,32 @@
 #ifndef PBR_LIGHTING_INCLUDED
 #define PBR_LIGHTING_INCLUDED
 
+#define DIELECTRICF0 half4(0.04, 0.04, 0.04, 1.0 - 0.04)
+
 const float PI = 3. 141592653589793
 const float NON_ZERO = 6.103515625e-5
 
-struct PBRData{
-
-}
+struct BRDFData {
+    half3 diffuseColor;
+    half3 specularColor;
+    half smoothness;
+    float3 normalWS;
+    float3 viewDirWS;
+};
 
 float3 SafeNormalize(float3 v) {
     return v * rsqrt(max(NON_ZERO, dot(v, v)));
+}
+
+half3 DisneyDiffuse(half roughness, half3 diffuseAlbedo, half ndotv, half ndotl, half ldoth) {
+    half fd90 = 0.5 + 2 * roughness * ldoth * ldoth;
+
+    half lightScatter = 1 + (fd90 - 1) * pow(1 - ndotl, 5);
+    half viewScatter = 1 + (fd90 - 1) * pow(1 - ndotv, 5);
+
+    diffuseAlbedo /= PI;
+
+    return diffuseAlbedo * lightScatter * viewScatter;
 }
 
 half3 Fresnel(half3 f0, half vdoth) {
@@ -31,7 +48,7 @@ float SmithJointGGX(float roughness, float ndotl, float ndotv) {
     return 1 / (2 * max(NON_ZERO, lambdaV + lambdaL));
 }
 
-half4 PBRLighting() {
+half4 PBRLighting(BRDFData brdfData) {
 
 }
 #endif
