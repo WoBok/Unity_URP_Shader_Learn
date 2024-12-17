6989d86ff3a8b6e2a7d230c036cb619869435d25
WoBok <darwin.vinci.wkc@gmail.com>
2024-12-17 14:03:55 +0800

    Batch Rename Tool

---
+++ Assets/Demos/BatchRenameTool/Editor/BatchRenameTool.cs
@@ -0,0 +1,43 @@
+using UnityEditor;
+using UnityEngine;
+
+public class BatchRenameTool : EditorWindow
+{
+
+    public string prefix = "Prefix";
+
+    [MenuItem("Tools/Batch Rename Tool")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(BatchRenameTool), false, "Batch Rename Tool");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Space(20);
+
+        prefix = EditorGUILayout.TextField("File Name Prefix:", prefix);
+
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Rename", GUILayout.Height(50)))
+        {
+            Rename();
+        }
+    }
+
+    public void Rename()
+    {
+        Object[] objects = Selection.GetFiltered(typeof(Object), SelectionMode.Assets);
+        int index = 1;
+        for (int i = 0; i < objects.Length; i++)
+        {
+            string path = AssetDatabase.GetAssetPath(objects[i]);
+            string newFileName = $"{prefix}_{index:00}";
+            AssetDatabase.RenameAsset(path, newFileName);
+            index++;
+        }
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+}
